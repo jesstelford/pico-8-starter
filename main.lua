26d6f12c9b31128589d52d911000e049daeb9c93
Jess Telford <jess+github@jes.st>
2022-04-17 15:57:33 +1000

    Move example debug dumping outside main loop

--- main.lua
+++ main.lua
@@ -8,11 +8,13 @@ function _update60()
   if (debugger.expand()) then return end
 end
 
+-- Dump to the terminal
+printTable({{x=1,y=2},{z=4,h="hello"}, 18}, true)
+
 function _draw()
   cls(0)
   print("pico-8 starter project",10,10,7)
   print("by jess telford",10,20,7)
-  printTable({{x=1,y=2},{z=4,h="hello"}, 18}, true)
   debugger.draw()
   sdbg()
 end
